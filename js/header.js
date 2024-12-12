b9377fbc0760feb411ced8d9bed374885a1e87c9
Kimbyul88 <byul88byul@gmail.com>
2024-12-12 12:26:00 +0900

    color 팝업 구현

--- js/header.js
+++ js/header.js
@@ -1,5 +1,4 @@
 const toolsInfoBtn = document.querySelector(".tools_info");
-
 const infoPopup = document.querySelector(".info-popup");
 
 toolsInfoBtn.addEventListener("click", () => {
@@ -13,4 +12,34 @@ toolsInfoBtn.addEventListener("click", () => {
     infoPopup.classList.remove("popup-hide");
     infoPopup.classList.add("popup-show");
   }
+});
+
+const toolsColorBtn = document.querySelector(".tools_color");
+const colorPopup = document.querySelector(".color-popup");
+
+toolsColorBtn.addEventListener("click", () => {
+  toolsColorBtn.classList.toggle("btn-active");
+  if (colorPopup.classList.contains("popup-show")) {
+    colorPopup.classList.add("popup-hide");
+    setTimeout(() => {
+      colorPopup.classList.remove("popup-show");
+    }, 500);
+    console.log(colorPopup);
+    console.log("hide");
+  } else {
+    colorPopup.classList.remove("popup-hide");
+    colorPopup.classList.add("popup-show");
+    console.log(colorPopup);
+
+    console.log("show");
+  }
+});
+
+const colorBtns = document.querySelectorAll(".color-btn");
+
+colorBtns.forEach((colorBtn) => {
+  colorBtn.addEventListener("click", () => {
+    const color = colorBtn.getAttribute("data-color");
+    document.documentElement.style.setProperty("--pink", color);
+  });
 });
